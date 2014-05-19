c6cd424f8c8f85c40afc919a6d802225342736b4
Invalid <wxdong5211@gmail.com>
2014-05-19 16:46:08 +0800

    add simple memory monitor

--- src/test/java/com/impler/pojbd/ExecMonitor.java
+++ src/test/java/com/impler/pojbd/ExecMonitor.java
@@ -5,10 +5,13 @@ package com.impler.pojbd;
  */
 public class ExecMonitor {
     private long time;
+    private long memory;
     public void start(){
+        memory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         time = System.currentTimeMillis();
     }
     public void stop(){
-        System.out.println("exec:"+ (System.currentTimeMillis()-time)+"ms");
+        System.out.println("exec:"+ (System.currentTimeMillis()-time)+"ms,"
+                +(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()-memory)/1024+"kb");
     }
 }
